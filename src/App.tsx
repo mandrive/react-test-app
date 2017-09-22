082b6185e91568b73e07a0bc1508dbc2c19f7a51
Maciej Kozera <maciek.kozera@gmail.com>
2017-09-22 20:06:42 +0200

    kekeke

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Witaj Grzegorzu</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To jest React, którego Ty, jako devops, nie ogarniesz! :D
         </p>
       </div>
     );
